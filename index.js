39f1c01e9c92a9f6ee9ef688d11bd14dd436499d
Muhammad Adnan <oymusafir@gmail.com>
2024-11-28 22:16:13 +0500

    done

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 4000;
+const filePath = "./data.json";
+
+app.use(express.json());
+
+function readData() {
+  return JSON.parse(fs.readFileSync(filePath, "utf-8"));
+}
+
+function writeData(data) {
+  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+}
+
+app.get("/data", (req, res) => {
+  try {
+    const data = readData();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json("Error reading data");
+  }
+});
+
+app.post("/data", (req, res) => {
+  try {
+    const currectData = readData();
+    const newData = { ...currectData, ...req.body };
+    writeData(newData);
+    res.status(200).json("Data Updated Sucessfully");
+  } catch (error) {
+    res.status(500).json("Error Updating Data");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`port is listening on ${port}`);
+});
